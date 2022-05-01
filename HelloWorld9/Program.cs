01be29989fe443f2723975408e498abfe14e8cd7
ROXANA lazarescu <roxana_lazarescu13@yahoo.com>
2022-05-01 23:14:10 +0300

    Add project files.

---
+++ HelloWorld9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Introduceti primul numar a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Introduceti al doilea numar b = ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("a={0}, b={1}", a, b);
+            int c = a; a = b; b = c;
+            Console.Write("Dupa interschimbare a={0}, b={1}", a, b); ;
+        }
+    }
+}
